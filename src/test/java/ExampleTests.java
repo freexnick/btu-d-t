37c7a55768a0af3931130aa6b9baa74367064e39
Nika <freexnick@yahoo.com>
2022-09-29 15:43:59 +0400

    01_task:code

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -3,20 +3,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ExampleTests {
 
     @Test
-    public void firstTest() {
+    public void firstTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
         WebElement button = driver.findElement(By.xpath("//ul/li/a"));
         button.click();
-        WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(), "No Test");
+        driver.navigate().back();
+        driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[11]/a")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/select/option[2]")).click();
+        Thread.sleep(10000);
+        driver.quit();
     }
 }
