00dd3c63f160ed665f23d64e70653976297ca1a1
Nika <freexnick@yahoo.com>
2022-10-29 21:59:27 +0400

    03_task_code

---
+++ src/test/java/Task3Tests.java
@@ -0,0 +1,29 @@
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class Task3Tests {
+  String URL = "https://bookstore.toolsqa.com/BookStore/v1/Books";
+
+  @Test
+  public void getStatusCode() {
+    given().when().get(URL).then().assertThat().statusCode(200);
+  }
+
+  @Test
+  public void checkLastBook() {
+    given().when()
+        .get(URL).then()
+        .body("books[-1].isbn", equalTo("9781593277574"));
+  }
+
+  @Test
+  public void checkPageCount() {
+    given().when()
+        .get(URL).then()
+        .body("books[0].pages", equalTo(234),
+            "books[1].pages", equalTo(254));
+  }
+
+}
